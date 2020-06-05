6f96752511ee3159c45eec19af6bf09fb7861935
unknown <jules.gioux@gmail.com>
2020-06-05 13:38:39 +0200

    first commit

---
+++ PHP_Exercice1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Partie 5 Exercice 1</title>
+</head>
+<body>
+    <?php
+
+    ?>
+    <p><?php echo ; ?></p>
+</body>
+</html>
